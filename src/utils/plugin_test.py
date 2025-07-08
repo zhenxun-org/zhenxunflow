f02c6f4384e7c1e173464ea4708d5803128dab89
HibiKier <775757368@qq.com>
2025-07-08 23:12:51 +0800

    :bug: 更新插件加载测试

--- src/utils/plugin_test.py
+++ src/utils/plugin_test.py
@@ -128,10 +128,10 @@ init()
 driver = get_driver()
 driver.register_adapter(OneBotV11Adapter)
 load_plugins("zhenxun/builtin_plugins")
-from zhenxun.builtin_plugins.plugin_store.data_source import ShopManage
+from zhenxun.builtin_plugins.plugin_store.data_source import ShopManager
 
 asyncio.run(
-    ShopManage.install_plugin_with_repo("{github_url}", "{module_path}", {is_dir}, True)
+    ShopManager.install_plugin_with_repo("{github_url}", "{module_path}", {is_dir}, True)
 )
 plugin = load_plugin(Path(__file__).parent / "zhenxun"/ "plugins" / "{module_name}")
 
